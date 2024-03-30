83857b62d8af5b4539371341fb08117904f3294b
FrostXia <62905307+FrostXia@users.noreply.github.com>
2024-03-30 18:01:02 -0400

    Add files via upload

---
+++ Prototype/ESPcam/espcam/espcam.ino
@@ -0,0 +1,58 @@
+#include "WifiCam.hpp"
+#include <WiFi.h>
+
+
+//Replace with your network credentials
+const char* ssid = "MaryBrownsUltraFastWiFi";
+const char* password = "chickengogogo";
+esp32cam::Resolution initialResolution;
+
+WebServer server(80);
+
+void
+setup() {
+  Serial.begin(115200);
+  Serial.println();
+  delay(2000);
+
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi failure %d\n", WiFi.status());
+    delay(5000);
+    ESP.restart();
+  }
+  Serial.println("WiFi connected");
+
+  {
+    using namespace esp32cam;
+
+    initialResolution = Resolution::find(1024, 768);
+
+    Config cfg;
+    cfg.setPins(pins::FreeNove);
+    cfg.setResolution(initialResolution);
+    cfg.setJpeg(80);
+
+    bool ok = Camera.begin(cfg);
+    if (!ok) {
+      Serial.println("camera initialize failure");
+      delay(5000);
+      ESP.restart();
+    }
+    Serial.println("camera initialize success");
+  }
+
+  Serial.println("camera starting");
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
+
+  addRequestHandlers();
+  server.begin();
+}
+
+void
+loop() {
+  server.handleClient();
+}
